3221ff68faf9de191884bccba213bd98e7c9a4f3
5JDR <dellarovere.sandro@gmail.com>
2023-06-09 11:16:19 +0200

    updated query 1

--- query_1.sql
+++ query_1.sql
@@ -1,4 +1,4 @@
-CREATE VIEW tpch1.suppliers_customers_same AS
+CREATE OR REPLACE VIEW tpch1.suppliers_customers_same AS
 	SELECT 
 		s_suppkey AS scs_suppkey,
 		c_custkey AS scs_custkey, 
@@ -9,19 +9,28 @@ CREATE VIEW tpch1.suppliers_customers_same AS
 		tpch1.supplier JOIN tpch1.nation ON s_nationkey = n_nationkey
 		JOIN tpch1.customer ON c_nationkey = n_nationkey
 	WHERE s_nationkey = c_nationkey;
-	
+
 SELECT
-	o_orderdate,
+	EXTRACT (YEAR FROM o_orderdate) AS _year,
+	EXTRACT (QUARTER FROM o_orderdate) AS _quarter,
+	EXTRACT (MONTH FROM o_orderdate) AS _month,
 	p_type,
 	scs_nationkey,
+	scs_nationname,
 	scs_regionkey,
+	r_name, 
 	SUM(l_extendedprice * (1 - l_discount)) AS revenue
 FROM
 	tpch1.lineitem JOIN tpch1.orders ON l_orderkey = o_orderkey
 	JOIN tpch1.suppliers_customers_same ON scs_suppkey = l_suppkey AND scs_custkey = o_custkey
 	JOIN tpch1.part ON l_partkey = p_partkey
+	JOIN tpch1.region ON scs_regionkey = r_regionkey
 GROUP BY 
-	o_orderdate,
+	_year,
+	_quarter,
+	_month,
 	p_type,
 	scs_nationkey,
-	scs_regionkey;
+	scs_nationname,
+	scs_regionkey,
+	r_name;
