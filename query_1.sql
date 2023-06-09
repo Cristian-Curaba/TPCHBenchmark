f173838ace441ed5774857478db2d43ee4187bf3
5JDR <dellarovere.sandro@gmail.com>
2023-06-09 11:04:42 +0200

    corrected query 1

--- query_1.sql
+++ query_1.sql
@@ -1,21 +1,27 @@
-CREATE MATERIALIZED VIEW tpch1.lineitem_orders(orderkey, custkey, suppkey, extendedprice, discount) AS
-	SELECT lineitem.l_orderkey, orders.o_custkey, lineitem.l_suppkey, lineitem.l_extendedprice, lineitem.l_discount
-	FROM tpch1.lineitem JOIN tpch1.orders ON lineitem.l_orderkey = orders.o_orderkey;
-
-CREATE MATERIALIZED VIEW tpch1.supplier_nk(suppkey, s_nationkey) AS
-	SELECT tpch1.supplier.s_suppkey, tpch1.supplier.s_nationkey
-	FROM tpch1.lineitem_orders JOIN tpch1.supplier ON lineitem_orders.suppkey = supplier.s_suppkey;
-
-CREATE MATERIALIZED VIEW tpch1.customer_nk(custkey, c_nationkey) AS
-	SELECT tpch1.customer.c_custkey, tpch1.customer.c_nationkey
-	FROM tpch1.lineitem_orders JOIN tpch1.customer ON lineitem_orders.custkey = customer.c_custkey;
-
-WITH supp_cust_nation AS (
-	SELECT suppkey, custkey, s_nationkey AS nationkey
-	FROM tpch1.supplier_nk JOIN tpch1.customer_nk ON s_nationkey = c_nationkey
-)
-
-SELECT lineitem_orders.extendedprice * (1 - lineitem_orders.discount) AS revenue
-FROM tpch1.lineitem_orders NATURAL JOIN supp_cust_nation;
-
-
+CREATE VIEW tpch1.suppliers_customers_same AS
+	SELECT 
+		s_suppkey AS scs_suppkey,
+		c_custkey AS scs_custkey, 
+		n_nationkey AS scs_nationkey,
+		n_name AS scs_nationname,
+		n_regionkey AS scs_regionkey
+	FROM
+		tpch1.supplier JOIN tpch1.nation ON s_nationkey = n_nationkey
+		JOIN tpch1.customer ON c_nationkey = n_nationkey
+	WHERE s_nationkey = c_nationkey;
+	
+SELECT
+	o_orderdate,
+	p_type,
+	scs_nationkey,
+	scs_regionkey,
+	SUM(l_extendedprice * (1 - l_discount)) AS revenue
+FROM
+	tpch1.lineitem JOIN tpch1.orders ON l_orderkey = o_orderkey
+	JOIN tpch1.suppliers_customers_same ON scs_suppkey = l_suppkey AND scs_custkey = o_custkey
+	JOIN tpch1.part ON l_partkey = p_partkey
+GROUP BY 
+	o_orderdate,
+	p_type,
+	scs_nationkey,
+	scs_regionkey;
